429f0dea8e485b12ccd24ba527d3235d544caf0d
Edgar <edgarignite1704@gmail.com>
2022-12-01 17:12:54 +0700

    update

--- Nam3_HK1/CSDL_SQL_Plus/Baitap/!TieuLuan/CodeIndex/src/Feature/ClusteringIndex.java
+++ Nam3_HK1/CSDL_SQL_Plus/Baitap/!TieuLuan/CodeIndex/src/Feature/ClusteringIndex.java
@@ -1,4 +1,20 @@
 package Feature;
 
+// nondense
 public class ClusteringIndex {
+    private int record, blockSize, recordSize, Vssn, blockPointer, zipCode;
+
+    public ClusteringIndex() {
+    }
+
+    public ClusteringIndex(int record, int blockSize, int recordSize, int vssn, int blockPointer, int zipCode) {
+        this.record = record;
+        this.blockSize = blockSize;
+        this.recordSize = recordSize;
+        Vssn = vssn;
+        this.blockPointer = blockPointer;
+        this.zipCode = zipCode;
+    }
+
+
 }
--- Nam3_HK1/CSDL_SQL_Plus/Baitap/!TieuLuan/CodeIndex/src/Feature/PrimaryIndex.java
+++ Nam3_HK1/CSDL_SQL_Plus/Baitap/!TieuLuan/CodeIndex/src/Feature/PrimaryIndex.java
@@ -1,4 +1,19 @@
 package Feature;
 
+// nondense
 public class PrimaryIndex {
+    private int record, blockSize, recordSize, Vssn, blockPointer;
+
+    public PrimaryIndex() {
+    }
+
+    public PrimaryIndex(int record, int blockSize, int recordSize, int vssn, int blockPointer) {
+        this.record = record;
+        this.blockSize = blockSize;
+        this.recordSize = recordSize;
+        this.Vssn = vssn;
+        this.blockPointer = blockPointer;
+    }
+
+
 }
